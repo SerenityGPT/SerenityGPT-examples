1181cdbef3af80d06d70dfd56754ec73993e5472
limon8884 <dbokatenko@nes.ru>
2025-08-19 15:16:14 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SerenityGPT-examples
+Here is an example of custom configuration of `serenity` directory.
+
+We use `Snapweaver` fake client to show how to configure SerenityGPT.
---
+++ clients/snapweaver/src/tickets_crawler.py
@@ -0,0 +1,24 @@
+import csv
+from pathlib import Path
+from typing import Optional
+from crawler.files.main import FilesCrawler, run as run_file_crawler
+from crawler.struct import ParsedDocument
+
+class TicketsCrawler(FilesCrawler):
+    def handle_custom(self, file: Path) -> list[ParsedDocument]:
+        results = []
+        with open(file, 'r') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                print(f"Processing ticket: {row['title']}")
+                results.append(ParsedDocument(
+                    title=row['title'],
+                    text=row['content'],
+                    url=row['url'],
+                    breadcrumbs=[row['project']],
+                    metadata={"ticket_id": row['id']},
+                ))
+        return results
+
+def run(conf):
+    run_file_crawler(conf, crawler_class=TicketsCrawler)
