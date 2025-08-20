70e12a15d0ee77e9b82539a3083af6bc39ea183e
limon8884 <dbokatenko@nes.ru>
2025-08-20 15:15:35 +0000

    refactor custom crawler

--- clients/demo/src/csv_crawler.py
+++ clients/demo/src/csv_crawler.py
@@ -1,26 +1,24 @@
 import csv
 from pathlib import Path
-from typing import Optional
-from crawler.files.main import FilesCrawler, run as run_file_crawler
+
+# has the fields described here https://docs.serenitygpt.com/configuration/global/#overview
+# and update_or_create method
 from crawler.struct import ParsedDocument
 
-class CSVCrawler(FilesCrawler):
-    def handle_custom(self, file: Path) -> list[ParsedDocument]:
-        if file.suffix != '.csv':
-            return []
-        results = []
+
+THIS_DIR = Path(__file__).parent
+def run(conf):
+    for file in THIS_DIR.glob('*.csv'):
+        print(f"Processing file: {file}")
         with open(file, 'r', encoding='utf-8') as f:
             reader = csv.DictReader(f)
             for row in reader:
                 print(f"Processing ticket: {row['title']}")
-                results.append(ParsedDocument(
+                doc = ParsedDocument(
                     title=row['title'],
                     text=row['content'],
                     url=row['url'],
                     breadcrumbs=[row['project']],
                     metadata={"ticket_id": row['id']},
-                ))
-        return results
-
-def run(conf):
-    run_file_crawler(conf, crawler_class=CSVCrawler)
+                )
+                doc.update_or_create(conf.source)
